0abc9163f90f21f1c369425b99f16ba2e8195b0f
JJ <jj@jj22.de>
2020-07-28 18:18:36 +0200

    1

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
 git pull
 cp -a ./src/main/java ./shell_versuch
-cd shell_versuch
-source source_mich
-javac Main.java
-java Main
+cd shell_versuch && source source_mich && javac Main.java && java Main
