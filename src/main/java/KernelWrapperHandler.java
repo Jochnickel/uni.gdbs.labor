051b865d744308398c86f3f5ef490bc54268953d
JJ <jj@jj22.de>
2020-07-28 18:02:21 +0200

    1

---
+++ src/main/java/KernelWrapperHandler.java
@@ -0,0 +1,44 @@
+/**
+ * a class to isolate the KernelWrapper
+ * @author Janke
+ *
+ */
+public class KernelWrapperHandler {
+
+	public static int fork() throws ForkFailedException {
+		try {
+			final var f = KernelWrapperDummy.fork();
+			if (f<0) {
+				throw new ForkFailedException();
+			}
+			return f;
+		}catch (ForkFailedException e) {
+			throw e;
+		}catch (Exception e) {
+			System.err.println("Please implement KernelWrapperHandler.fork");
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	public static int execv(String[] userInput) {
+		try {
+			return KernelWrapperDummy.execv(System.getProperty("user.dir"), userInput);
+		}catch (Exception e) {
+			System.err.println("Please implement KernelWrapperHandler.execv");
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	public static int waitpid(int myChildOrNull) {
+		try {
+			return KernelWrapperDummy.waitpid(myChildOrNull, null, 0);
+		}catch (Exception e) {
+			System.err.println("Please implement KernelWrapperHandler.waitpid");
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+}
---
+++ src/main/java/ProgramNotFoundException.java
@@ -0,0 +1,9 @@
+
+public class ProgramNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
