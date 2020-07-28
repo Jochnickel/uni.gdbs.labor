1063e7b8482934816cbd0f5f0f85e41781b7f383
Your Name <you@example.com>
2020-07-28 14:18:57 +0000

    1

--- shell_versuch/Main.java
+++ shell_versuch/Main.java
@@ -2,7 +2,36 @@ import static cTools.KernelWrapper.*;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
-    exit(0);
-  }
+		printWelcome();
+		for (;;) {
+			try {
+				printCursor();
+				final var userInput = readInput();
+				execInput(userInput);
+			} catch (ExitShellException e) {
+				break;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void execInput(List<String> userInput) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static List<String> readInput() throws ExitShellException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static void printCursor() {
+		System.out.printf(">_");
+
+	}
+
+	private static void printWelcome() {
+		System.out.printf("Welcome to the minimal Shell!\n");
+	}
 }
