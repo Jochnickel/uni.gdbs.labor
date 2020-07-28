5f29ab7158bb6702338f68bed125556bdcf3fce0
JJ <jj@jj22.de>
2020-07-28 20:36:01 +0200

    1

--- shell_versuch/Main.java
+++ shell_versuch/Main.java
@@ -7,7 +7,7 @@ class Main {
 	final static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
+		System.out.println("".split("").length);
 		printWelcome();
 		for (;;) {
 			try {
