d4d55d9745b9d331e3f376550379c632ec8971c6
Your Name <you@example.com>
2020-08-14 18:53:56 +0200

    auto

--- sync/fuenf_phil.c
+++ sync/fuenf_phil.c
@@ -59,7 +59,7 @@ int staebchen_nehmen(int my_id, int pos) {
   if (n==1) {
     printf("%i nimmt %i\n", my_id, pos);
 //    occupied[pos] = my_id;
-    printf("[%d,%d,%d,%d,%d]\n",occupied[0],occupied[1],occupied[2],occupied[3],occupied[4]);
+//    printf("[%d,%d,%d,%d,%d]\n",occupied[0],occupied[1],occupied[2],occupied[3],occupied[4]);
     staebchen[pos]--; // ergibt 0, gibt aber chance zur fehlererkennung
     sem_v(sammy[pos]);
     return 1;
