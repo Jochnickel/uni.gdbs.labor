abcff260d14ac6b56fce65a30ed32a397e48322e
Your Name <you@example.com>
2020-08-14 19:40:28 +0200

    auto

--- sync/xxx.c
+++ sync/xxx.c
@@ -26,7 +26,7 @@ void test_setup(void) {
   printf("Test Setup\n");
   readers=5;
   writers=1;
-  readersAreRdy = sem_init(1-readers);
+  readersAreRdy = sem_init(1);
   writerDone = sem_init(0);
 }
 
