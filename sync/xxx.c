4e1d08cc7a7ac37d09302d8161e1929f171613f6
Your Name <you@example.com>
2020-08-14 19:29:08 +0200

    auto

--- sync/xxx.c
+++ sync/xxx.c
@@ -63,10 +63,12 @@ void reader(long my_id) {
 
 void writer(long long_my_id) {
 	for(;increase_me<LIMIT;){
+
 		sem_p(readerRdy);
 		sem_v(readerRdy);
 
 		increase_me++;
 		sem_v(writerDone);
+
 	}
 }
