2fcd0ef6b0b952f0903db61ca70b57cc81f288a7
Your Name <you@example.com>
2020-08-14 19:02:19 +0200

    auto

--- sync/xxx.c
+++ sync/xxx.c
@@ -44,44 +44,5 @@ void reader(long my_id) {
 
 
 void writer(long long_my_id) {
-  int my_id=long_my_id;
-  int nxt=(my_id+1)%5;
-
-  int i=100; // will so oft was essen
-  int links=0;  // welche staebchen habe ich gerade
-  int rechts=0;
-  while (i>0) {
-    // probiere zufaellig staebchen zu nehmen, die noch nicht da sind
-    if (!links && random()%10==7) links=staebchen_nehmen(my_id, my_id);
-    if (!rechts && random()%10==7) rechts=staebchen_nehmen(my_id, nxt);
-
-    // wenn beide da sind: essen!
-    if (links && rechts) {
-      printf("%i futtert jetzt\n", my_id);
-      usleep(random()%200);
-      i--;
-      // staebchen wieder hinlegen
-      staebchen_weglegen(my_id, my_id); links=0;
-      staebchen_weglegen(my_id, nxt);   rechts=0;
-    }
-
-    // fuer die deadlock-erkennung melden, wenn ein staebchen von mir
-    // belegt ist (beide koennen hier nicht belegt sein, dann wurde gegessen)
-    have_one[my_id] = links || rechts;
-    if (have_one[0]+have_one[1]+have_one[2]+have_one[3]+have_one[4]==5) {
-      //printf("Deadlock!\n");
-      //exit(1);
-      printf("%i gibt freiwillig ab\n", my_id);
-      if (links) {
-        staebchen_weglegen(my_id, my_id); links=0;
-      }
-      if (rechts) {
-        staebchen_weglegen(my_id, nxt);   rechts=0;
-      }
-      have_one[my_id] = 0;
-    }
-
-    // denken
-    usleep(random()%200);
-  }
+k  }
 }
